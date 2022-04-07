9c5ddc6d6d962ed3e7d55418376e0bb07bae1967
KRILLE123 <42100456+KRILLE123@users.noreply.github.com>
2022-04-07 10:13:11 +0200

    aa
    dd

---
+++ Assets/zombie_movement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zombie_movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    GameObject target;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 diff = Vector3.Normalize(target.transform.position - transform.position) * Time.deltaTime;
+        transform.position += diff;
+    }
+}
